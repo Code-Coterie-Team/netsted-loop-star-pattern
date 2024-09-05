7c676bea1cb147c8bdcca0de754886244cee693c
K.rahimi <kimiarhimi79@gmail.com>
2024-09-05 15:29:43 +0330

    nested star

--- star.js
+++ star.js
@@ -1,11 +1,8 @@
 function StarsLoop(){
-    let star = "* "
-    for (let i = 1; i <= 10; i++) {  
-        let pattern = '';  
-        for (let j = 1; j <= i; j++) {  
-            pattern += star; 
-        }  
-        console.log(pattern);  
+    let star = "" ;
+    for (let i = 1; i <= 10; i++) { 
+        star += "* ";
+        console.log(star);  
     }
 
 }
